02de100f8e0248f12e94313a9435499d6a621500
kinkyhumans <takepart@kinkyhumans.com>
2020-07-23 14:16:54 +0100

    Updates

---
+++ precache-manifest.45c756b8cec61e38d005e70e5d347442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39e169a645d15166895b0b7eef9698d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0cca34a2ed8986411ca",
+    "url": "/static/css/main.4139be80.chunk.css"
+  },
+  {
+    "revision": "11f64763ee7dde5b0f40",
+    "url": "/static/js/2.ecd406b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecd406b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0cca34a2ed8986411ca",
+    "url": "/static/js/main.a5b9e69b.chunk.js"
+  },
+  {
+    "revision": "4e0721556192c73bc4ac",
+    "url": "/static/js/runtime-main.83afaa65.js"
+  },
+  {
+    "revision": "669d0a0491ca51a367bd5b65b44a4ab0",
+    "url": "/static/media/logo.669d0a04.png"
+  }
+]);
